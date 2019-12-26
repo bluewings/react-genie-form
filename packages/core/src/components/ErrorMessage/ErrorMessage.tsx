937e3deabe66f7c7b7a0ca4e561f8dcf9880433a
sungwon.cha <mygenie3@naver.com>
2019-12-26 10:49:15 +0900

    bugfix

--- packages/core/src/components/ErrorMessage/ErrorMessage.tsx
+++ packages/core/src/components/ErrorMessage/ErrorMessage.tsx
@@ -4,7 +4,7 @@ import { get } from 'lodash-es';
 
 function ErrorMessage({ className, errors }: any) {
   const message = useMemo(() => get(errors, [0, 'message']), [errors]);
-  return message && <p className={className}>{message}</p>;
+  return message ? <p className={className}>{message}</p> : null;
 }
 
 export default ErrorMessage;
