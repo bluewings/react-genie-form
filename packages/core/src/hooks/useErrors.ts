eb6093daa0a598196ab3dd4b016b51acc4a1ab8f
sungwon.cha <mygenie3@naver.com>
2019-12-26 13:43:38 +0900

    fix

--- packages/core/src/hooks/useErrors.ts
+++ packages/core/src/hooks/useErrors.ts
@@ -2,6 +2,12 @@ import { get } from 'lodash-es';
 import useContext from './useContext';
 import { useMemo } from 'react';
 
+const messageFirst = (a: any, b: any) => {
+  const c = a.message ? 1 : 0;
+  const d = b.message ? 1 : 0;
+  return c === d ? 0 : c > d ? -1 : 1;
+};
+
 function useErrors(dataPath: string, schema: any) {
   const value = useContext('errors');
   const dataPaths = useMemo(
@@ -18,7 +24,8 @@ function useErrors(dataPath: string, schema: any) {
         ],
         [],
       )
-      .filter((e) => e);
+      .filter((e) => e)
+      .sort(messageFirst);
     return errors.length > 0 ? errors : undefined;
   }, [value, dataPaths]);
 }
