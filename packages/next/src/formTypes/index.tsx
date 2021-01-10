006736ad12b320cffcf98c2425c93694c0478ed0
sungwon-cha <mygenie3@naver.com>
2021-01-10 12:06:27 +0900

    add rollup build scripts

--- packages/next/src/formTypes/index.tsx
+++ packages/next/src/formTypes/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import TypeArray from './TypeArray';
 import TypeBoolean from './TypeBoolean';
 import TypeDate from './TypeDate';
---
+++ packages/next/src/index.ts
@@ -0,0 +1,6 @@
+import Form from './components/Form';
+// import TypeArray from './formTypes/TypeArray';
+
+export default Form;
+
+// export { TypeArray };
