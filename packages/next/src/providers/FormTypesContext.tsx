9249d3ff4d4edb2632ec893f3387a9da66984eb4
sungwon.cha <mygenie3@naver.com>
2021-04-09 20:56:04 +0900

    fix

--- packages/next/src/providers/FormTypesContext.tsx
+++ packages/next/src/providers/FormTypesContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useMemo } from 'react';
-import baseFormTypes from '../formTypes';
 import ErrorBoundary from '../components/ErrorBoundary';
+import baseFormTypes from '../formTypes';
 import { useFormTypes } from '../hooks';
 
 const FormTypesContext = createContext<any>({});
@@ -9,19 +9,26 @@ const Provider = ({ formTypes, formTypeMap, children }: any) => {
   const mergedFormTypes = useMemo(
     () =>
       [
-        ...Object.entries(formTypeMap || {}).map(([path, Component]: any) => ({
+        ...Object.entries(formTypeMap || {}).map(([path, component]: any) => ({
           test: (hint: any) => hint.path === path,
+          component,
+        })),
+        ...(formTypes || []),
+        ...baseFormTypes,
+      ].filter(Boolean).filter(e => e.component)
+      .map((e: any) => {
+        const Component: any = e.component
+        return {
+          ...e,
           component: (props: any) => {
             return (
               <ErrorBoundary>
                 <Component {...props} />
               </ErrorBoundary>
             )
-          },
-        })),
-        ...(formTypes || []),
-        ...baseFormTypes,
-      ].filter(Boolean),
+          }
+        }
+      }),
     [formTypes, formTypeMap],
   );
 
