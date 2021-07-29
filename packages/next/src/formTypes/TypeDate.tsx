ab5e1db93c58e80d8b41044874a045d6b49a3299
sungwon-cha <mygenie3@naver.com>
2021-07-29 16:35:39 +0900

    fix

--- packages/next/src/formTypes/TypeDate.tsx
+++ packages/next/src/formTypes/TypeDate.tsx
@@ -1,8 +1,8 @@
 import React, { useMemo } from 'react';
 
-function TypeDate(pro: any) {
-  const { name, value, onChange, schema } = pro;
-  console.log(pro);
+function TypeDate(props: any) {
+  const { name, value, onChange, schema } = props;
+  // console.log(props);
 
   const type = useMemo(() => schema?.format || 'date', [schema]);
   return <input type={type} name={name} value={value} onChange={onChange} />;
